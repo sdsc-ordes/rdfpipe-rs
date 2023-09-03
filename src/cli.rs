f0f824589e913e601397d22a224d1119025cdf33
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-04 01:34:47 +0200

    refactor: use oxigraph helpers

--- src/cli.rs
+++ src/cli.rs
@@ -1,29 +1,24 @@
-use clap::{Parser, ValueEnum};
-use std::path::PathBuf;
+use clap::Parser;
+use clap::ValueEnum;
+use oxigraph::io::GraphFormat;
 
-#[derive(ValueEnum, Copy, Clone, Debug)]
-enum Format {
-    Turtle,
-    NTriples,
-}
-
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, ValueEnum, Parser, Debug)]
 #[command(author, about = "RDF conversion tool")]
-pub struct Args {
+pub(crate) struct Args {
     #[arg(
         short,
         long,
         default_value = "turtle",
         help = "Input RDF serialization format"
     )]
-    input_format: Option<Format>,
+    pub(crate) input_format: Option<GraphFormat>,
     #[arg(
         short,
         long,
         default_value = "turtle",
         help = "Output RDF serialization format"
     )]
-    output_format: Option<Format>,
+    pub(crate) output_format: Option<GraphFormat>,
     #[arg(default_value = "-", help = "Input file. Use - for stdin.")]
-    input_file: Option<String>,
+    pub(crate) input_file: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod cli;
+mod utils;
 
 use crate::cli::Args;
 use clap::Parser;
-use sophia::parser::{nt::NTriplesParser, turtle::TurtleParser};
-use sophia::serializer::{nt::NtSerializer, turtle::TurtleSerializer};
-use sophia::triple::stream::TripleSource;
 
 // Should replicate rdfpipe's command line interface
 // rdfpipe [-i {ttl,nt}] [-o {ttl,nt}] [input]
@@ -13,5 +11,9 @@ use sophia::triple::stream::TripleSource;
 
 fn main() {
     let args = Args::parse();
+    if let Some("-") = args.input_file.as_deref() {
+        println!("Reading from stdin");
+    }
+
     println!("{:?}", args);
 }
