ce29495b69a57349bc90344198712b79cc57e58d
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-08 01:00:41 +0200

    feat(main): support stdin and file input

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,40 @@ mod utils;
 use crate::cli::Args;
 use clap::Parser;
 use oxigraph::io::GraphFormat;
-use std::io;
+use std::io::{self, Stdin, Read, BufRead, BufReader};
 use std::fs::File;
+use std::error::Error;
 
 // Should replicate rdfpipe's command line interface
 // rdfpipe [-i {ttl,nt}] [-o {ttl,nt}] [input]
 // if input is - (default) or missing, read from stdin and infer format from file contents
 // if an input file is specified, infer format from file extension
 
-fn main() {
-    let args = Args::parse();
-    if let Some("-") = args.input_file.as_deref() {
-        println!("Reading from stdin");
+
+fn get_input_buf(input_path: Option<&str>) -> io::Result<Box<dyn BufRead>> {
+    match input_path {
+        Some("-") | None => {
+            // Read from stdin
+            let stdin = io::stdin();
+            let handle = stdin.lock();
+            Ok(Box::new(handle))
+        }
+        Some(file_path) => {
+            // Attempt to open and read from file
+            let file = File::open(file_path)?;
+            let reader = BufReader::new(file);
+            Ok(Box::new(reader))
+        }
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
     
-    let input = match args.input_file {
-        Some(x) => File::open(x),
-        Some("-") | None => io::stdin(),
-        _ => panic!("no valid input")
-    };
+    let input_buf = get_input_buf(args.input_file.as_deref())?;
 
-    let input_buffer = io::BufReader::new(input);
-
-        
-    utils::parse_any_rdf(input_buffer, GraphFormat::ttl);
+    utils::parse_any_rdf(input_buf, GraphFormat::Turtle);
 
     println!("{:?}", args);
+    Ok(())
 }
