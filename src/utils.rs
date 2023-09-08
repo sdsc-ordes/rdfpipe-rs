5614dd9b3d135ba0703a181c7c78dc650c8f0868
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-08 22:40:07 +0200

    feat: add serializer

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use oxigraph::io::read::TripleReader;
-use oxigraph::io::{GraphFormat, GraphParser};
-use std::io::BufRead;
+use oxigraph::io::{GraphFormat, GraphParser, GraphSerializer};
+use std::io::{self, BufRead};
 
 // Call the appropriate parser based on the input RDF format.
 pub fn parse_any_rdf<R: BufRead>(
@@ -8,4 +8,11 @@ pub fn parse_any_rdf<R: BufRead>(
     format: GraphFormat,
 ) -> Result<TripleReader<R>, oxigraph::io::read::ParseError> {
     GraphParser::from_format(format).read_triples(src)
+}
+
+pub fn serialize_any_rdf<W: std::io::Write>(
+    dest: W,
+    format: GraphFormat,
+) -> Result<oxigraph::io::write::TripleWriter<W>, io::Error> {
+    GraphSerializer::from_format(format).triple_writer(dest)
 }
