9643215ef8cb27d7b024c9f9231673a515c5718a
cmdoret <cyril.mattheydoret@gmail.com>
2023-09-09 03:00:50 +0200

    feat(cli): input format inference logic

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod utils;
 
 use crate::cli::Args;
 use clap::Parser;
+use oxigraph::io::read::ParseError;
 use oxigraph::io::GraphFormat;
-use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+use std::path::Path;
 
 // Should replicate rdfpipe's command line interface
 // rdfpipe [-i {ttl,nt}] [-o {ttl,nt}] [input]
@@ -29,13 +30,29 @@ fn get_input_buf(input_path: Option<&str>) -> io::Result<Box<dyn BufRead>> {
         }
     }
 }
+// Infer RDF serialization format from file extension
+// If the extension is missing or unknown, None is returned.
+fn format_from_path<'a>(path: &'a str) -> Option<GraphFormat> {
+    let ext = Path::new(path).extension()?.to_str()?;
+    GraphFormat::from_extension(ext)
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+    // If input specified in CLI, use that format
+    // Otherwise, infer format from file extension
+    let input_format = match args.input_format {
+        Some(format) => GraphFormat::from(&format),
+        None => args
+            .input_file
+            .as_ref()
+            .and_then(|path| format_from_path(path))
+            .ok_or_else(|| "Could not infer input format")?,
+    };
 
     let input_buf = get_input_buf(args.input_file.as_deref())?;
-    let input_format = GraphFormat::from(args.input_format.clone().unwrap());
-    let output_format = GraphFormat::from(args.output_format.clone().unwrap());
+
+    let output_format = GraphFormat::from(&args.output_format.unwrap());
     let parser = utils::parse_any_rdf(input_buf, input_format)?;
     let mut writer = utils::serialize_any_rdf(std::io::stdout(), output_format)?;
     for triple in parser {
@@ -43,6 +60,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     writer.finish()?;
 
-    println!("{:?}", args);
     Ok(())
 }
