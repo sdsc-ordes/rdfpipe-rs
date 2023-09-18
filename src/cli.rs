6189f2ef827bb540341860181b89faa7aab94abc
Cyril Matthey-Doret <cyril.mattheydoret@gmail.com>
2023-09-18 16:37:26 +0000

    feat(cli): flexible formats (#8)
    * feat(cli): impl ValueEnum for ArgGraphFormat
    
    * refactor(cli): define aliases via clap derive

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,19 @@
+use std::fmt::Error;
+use std::str::FromStr;
+
 use clap::Parser;
 use clap::ValueEnum;
 use oxigraph::io::GraphFormat;
 
 // This lets clap automate validation of
 // RDF formats from the command line
-#[derive(ValueEnum, Clone, Debug)]
+#[derive(Clone, Debug, ValueEnum)]
 pub(crate) enum ArgGraphFormat {
+    #[clap(alias = "ttl")]
     Turtle,
+    #[clap(alias = "nt", alias = "ntriples")]
     NTriples,
+    #[clap(alias = "xml", alias = "rdf/xml")]
     RdfXml,
 }
 
@@ -23,15 +29,22 @@ impl From<&ArgGraphFormat> for GraphFormat {
     }
 }
 
+impl FromStr for ArgGraphFormat {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ntriples" | "nt" | "n-triples" => Ok(ArgGraphFormat::NTriples),
+            "xml" | "rdf/xml" | "rdf-xml" => Ok(ArgGraphFormat::RdfXml),
+            "ttl" | "turtle" => Ok(ArgGraphFormat::Turtle),
+            _ => Err(Error),
+        }
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(author, about = "RDF conversion tool")]
 pub(crate) struct Args {
-    #[arg(
-        short,
-        long,
-        default_value = "turtle",
-        help = "Input RDF serialization format"
-    )]
+    #[arg(short, long, help = "Input RDF serialization format")]
     pub(crate) input_format: Option<ArgGraphFormat>,
     #[arg(
         short,
